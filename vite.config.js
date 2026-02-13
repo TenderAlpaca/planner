4686f85fc7e8b649c274f514e2f33c14bb3ddcd5
Marko <45314552+TenderAlpaca@users.noreply.github.com>
2026-02-13 12:46:03 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/date-planner/', // Replace with your repo name
+  base: '/planner/', // Replace with your repo name
 })
